a7c8440fce351c0cb524055b6695f560713c610d
aniketsingh1023 <aniketsinghn10@gmail.com>
2025-11-02 14:21:22 +0530

    added 2d array

---
+++ arrays/two.java
@@ -0,0 +1,22 @@
+public class two{
+
+    public static void main(String[] args) {
+       /* 
+        
+            1 2 3 
+            4 5 6 
+            7 8 9 
+
+
+
+        */
+        int [][] arr = {
+
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        System.out.println(arr);
+        
+    }
+}
